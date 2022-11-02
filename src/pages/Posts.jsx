638afca4c900c4be2a9e8d256008cf849b8a41db
JA9901 <jareyes999@gmail.com>
2022-11-02 16:49:10 -0400

    added

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -5,11 +5,20 @@ import axios from "axios";
 const Posts = () => {
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchId, setSearchId] = useState(id);
+
+  function onSearch() {
+    console.log(searchId);
+  }
 
   useEffect(() => {
     async function fetchPosts() {
-      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+      );
       setPosts(data);
+      setLoading(false);
     }
     fetchPosts();
   }, []);
@@ -20,22 +29,27 @@ const Posts = () => {
         <button>← Back</button>
         <div className="post__search--container">
           <label className="post__search--label">Search by Id</label>
-          <input type="number" />
-          <button>Enter</button>
+          <input type="number" value={searchId} onChange={(event) => setSearchId(event.target.value)} />
+          <button onClick={() => onSearch()}>Enter</button>
         </div>
       </div>
-      <div className="post">
-        <div className="post__title">post.title</div>
-        <p className="post__body">post.body</p>
-      </div>
-      <div className="post">
-        <div className="post__title">
-          <div className="post__title--skeleton"></div>
+      {loading ? 
+        new Array(10).fill(0).map((_, index) => (
+        <div className="post" key={index}>
+          <div className="post__title">
+            <div className="post__title--skeleton"></div>
+          </div>
+          <div className="post__body">
+            <p className="post__body--skeleton"></p>
+          </div>
         </div>
-        <div className="post__body">
-          <p className="post__body--skeleton"></p>
-        </div>
-      </div>
+      ))
+      : posts.map(post => (
+          <div className="post" key={post.id}>
+            <div className="post__title">{post.title}</div>
+            <p className="post__body">{post.body}</p>
+          </div>
+        ))}
     </>
   );
 };
